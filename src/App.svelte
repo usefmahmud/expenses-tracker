465548d6cc783d71527976e7f3fbc530b3b9ece1
Yousef Mahmoud <usef.mahmoud@outlook.com>
2025-01-02 12:10:18 +0200

    init: Initialize the project files.

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  let msg = 'Hello, World'
+  let isMsgShown = false
+
+</script>
+
+<button onclick={() => isMsgShown = !isMsgShown}>
+  { isMsgShown ? 'Hide' : 'Show' }
+</button>
+<h1>
+  { isMsgShown ? msg : '' }
+</h1>
