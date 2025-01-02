0712ee455db4e90c5236f4cc0ea1753d04a433c9
Yousef Mahmoud <usef.mahmoud@outlook.com>
2025-01-02 12:22:00 +0200

    feat: Set the main routes.

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,21 @@
 <script lang="ts">
+  import { Router, Link, Route } from "svelte-routing"
+  import Dashboard from "./routes/Dashboard.svelte"
+  import Settings from "./routes/Settings.svelte"
+
   let msg = 'Hello, World'
   let isMsgShown = false
 
 </script>
 
-<button onclick={() => isMsgShown = !isMsgShown}>
-  { isMsgShown ? 'Hide' : 'Show' }
-</button>
-<h1>
-  { isMsgShown ? msg : '' }
-</h1>
+<Router>
+  <button onclick={() => isMsgShown = !isMsgShown}>
+    { isMsgShown ? 'Hide' : 'Show' }
+  </button>
+  <h1>
+    { isMsgShown ? msg : '' }
+  </h1>
+
+  <Route path="/" component={Dashboard} />
+  <Route path="/settings" component={Settings} />
+</Router>
